4ca8bc87072077f3b737d896b413da7171f40894
1000ch <shogo.sensui@gmail.com>
2015-06-15 00:00:49 +0900

    use ls-path module

--- cmd.js
+++ cmd.js
@@ -3,7 +3,8 @@
 var fs             = require('fs');
 var path           = require('path');
 var minimist       = require('minimist');
-var glob           = require('glob');
+var lspath         = require('ls-path');
+var isMarkdown     = require('is-md');
 var async          = require('async');
 var GitHubMarkdown = require('./');
 var package        = require('./package');
@@ -26,58 +27,44 @@ if (argv.version) {
   process.exit();
 }
 
-var targets = [];
+lspath(argv._, function (error, paths) {
 
-argv._.filter(function (arg) {
-  return fs.existsSync(arg);
-}).forEach(function (arg) {
-  if (fs.statSync(arg).isFile()) {
-    targets.push(arg);
-  } else if (fs.statSync(arg).isDirectory()) {
-    fs.readdirSync(arg).forEach(function (file) {
-      targets.push(file);
-    });
-  } else {
-    glob(arg, function (error, files) {
-      files.forEach(function (file) {
-        targets.push(file);
-      });
-    });
-  }
-});
-
-async.each(targets, function (file, index, files) {
-
-  var config = {
-    title: argv.title || path.basename(file, '.md'),
-    file: file,
-    template: argv.template
-  };
-
-  var dest;
-  if (argv.dest) {
-    dest = path.join(
-      path.dirname(argv.dest), 
-      path.basename(file, '.md') + '.html'
-    );
-  } else {
-    dest = path.join(
-      process.cwd(),
-      path.basename(file, '.md') + '.html'
-    );
-  }
-
-  var ghmd = new GitHubMarkdown(config);
-  ghmd.render().then(function (html) {
-    fs.writeFileSync(dest, html, {
-      encoding: 'utf8',
-      flag: 'w'
-    });
+  var markdowns = paths.filter(function (p) {
+    return isMarkdown(p);
   });
 
-}, function (error, result) {
-  if (error) {
-    throw error;
-  }
-});
+  async.each(markdowns, function (file, index, files) {
 
+    var config = {
+      title: argv.title || path.basename(file, '.md'),
+      file: file,
+      template: argv.template
+    };
+
+    var dest;
+    if (argv.dest) {
+      dest = path.join(
+        path.dirname(argv.dest),
+        path.basename(file, '.md') + '.html'
+      );
+    } else {
+      dest = path.join(
+        process.cwd(),
+        path.basename(file, '.md') + '.html'
+      );
+    }
+
+    var ghmd = new GitHubMarkdown(config);
+    ghmd.render().then(function (html) {
+      fs.writeFileSync(dest, html, {
+        encoding: 'utf8',
+        flag: 'w'
+      });
+    });
+
+  }, function (error, result) {
+    if (error) {
+      throw error;
+    }
+  });
+});
