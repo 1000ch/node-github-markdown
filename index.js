428d51b6610037ce511af2efc6dda88c9fa8e16e
CZYK <danko@czyk.nl>
2017-12-14 14:38:34 +0100

    Enable html in order to support tags like `<sup>`

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = (markdown, config = {}) => {
 
   const markdownIt = new MarkdownIt({
     langPrefix: 'hljs ',
+    html: true,
     highlight: (string, lang) => {
       try {
         if (lang) {
